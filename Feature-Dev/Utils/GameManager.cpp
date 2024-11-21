9abbc78c39773fa966cb8ebbc401419d2c5b13e5
Lê Anh Duy <duyla25clc@gmail.com>
2024-11-21 14:28:00 +0700

    oki

--- Feature-Dev/Utils/GameManager.cpp
+++ Feature-Dev/Utils/GameManager.cpp
@@ -1,1 +1,51 @@
 #include "GameManager.h"
+
+
+void GameManager::init(sf::RenderWindow& window) {
+    // load texture first
+    myTextureManager = &TextureManager::getInstance();
+    myTextureManager->loadNewTexture("Assets/Texture/Texture_Pack/mario.tsx", "Assets/Texture/Texture_Pack/mario.png");
+
+
+    game = &GameController::getInstance();
+    player = new Player(sf::Vector2f(50, 50), sf::Vector2f(14, 14));
+
+    gameMap = new Map();
+    gameMap->loadMap("Assets/map.txt", player);
+
+    player->setMap(gameMap);
+    game->setPlayer(player);
+    game->setMap(gameMap);
+    camera = new Camera(window);
+    game->startGame();
+    this->window = &window;
+}
+
+
+void GameManager::play(float dt) {
+    window->clear(sf::Color::Black);
+
+    // check all the window's events that were triggered since the last iteration of the loop
+    sf::Event event;
+    while (window->pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+            window->close();
+    }
+    camera->followPlayer(player->getPos().x, player->getPos().y, player->getHitbox().getSize().x, player->getHitbox().getSize().y);
+
+    camera->setCameraView(*window);
+
+    // clear the window with black color
+    game->update(event, dt);
+    game->render(*window);
+    window->display();
+}
+
+GameManager::~GameManager() {
+    //		delete game;
+    delete camera;
+    delete gameMap;
+    delete player;
+
+}
--- Feature-Dev/Utils/GameManager.h
+++ Feature-Dev/Utils/GameManager.h
@@ -10,68 +10,23 @@
 
 using namespace std;
 
-class GameManager
-{
+class GameManager {
 private:
-	GameController* game = NULL;
-	vector <string> mapPath;
-	int currentMap = 0;
-	Camera* camera = NULL;
-	Map* gameMap = NULL;
-	sf::RenderWindow* window;
-	Player* player = NULL;
     TextureManager* myTextureManager = NULL;
+	GameController* game = NULL;
+	sf::RenderWindow* window;
+	Camera* camera = NULL;
+
+	vector <string> mapPath;
+	Map* gameMap = NULL;
+	int currentMap = 0;
+	
+    Player* player = NULL;
 
 
 public:
-	~GameManager() {
-//		delete game;
-		delete camera;
-		delete gameMap;
-		delete player;
-
-	}
-
-	void init(sf::RenderWindow& window) {
-        // load texture first
-        myTextureManager = &TextureManager::getInstance();
-        myTextureManager->loadNewTexture("Assets/Texture/Texture_Pack/mario.tsx", "Assets/Texture/Texture_Pack/mario.png");
-
-
-
-        game = &GameController::getInstance();
-        player = new Player(sf::Vector2f(50, 50), sf::Vector2f(14, 14));
-
-        gameMap = new Map();
-        gameMap->loadMap("Assets/map.txt", player);
-
-        player->setMap(gameMap);
-        game->setPlayer(player);
-        game->setMap(gameMap);
-        camera = new Camera(window);
-        game->startGame();
-		this->window = &window;
-
-	}
-
-	void play(float dt) {
-        window->clear(sf::Color::Black);
-
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window->pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window->close();
-        }
-        camera->followPlayer(player->getPos().x, player->getPos().y, player->getHitbox().getSize().x, player->getHitbox().getSize().y);
-
-        camera->setCameraView(*window);
-
-        // clear the window with black color
-        game->update(event, dt);
-        game->render(*window);
-        window->display();
-	}
+	~GameManager();
+    void init(sf::RenderWindow& window);
+	void play(float dt);
 };
 
